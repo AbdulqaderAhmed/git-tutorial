91b875784e4f943bd4a1835a8af9efbff26eae3f
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-12-17 15:14:21 +0300

    changing filename

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './assets/app.css'
 export default function App() {
   return (
     <div>
-        <h1 className="text-3xl font-bold text-center underline">App2</h1>
+        <h1 className="text-3xl font-bold text-center underline">App</h1>
     </div>
   )
 }
