b403bcca213f008d3dc4a0678bc49e01db02b787
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-12-17 15:46:07 +0300

    update text inside master index

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-        <h1>this is an index page.</h1>
+        <h1>this is master branch.</h1>
     </div>
   )
 }
