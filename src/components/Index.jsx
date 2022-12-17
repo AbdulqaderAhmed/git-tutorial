f30fe89ff99be27f78485d038b61c8edffd979c4
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-12-17 18:08:43 +0300

    adfsdf

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,7 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 export default function Index() {
-  
+  const [isBottom, setIsBottom] = useState(false);
+
+  const handleScroll = () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
+
+    if (bottom) {
+      setIsBottom(true);
+    } else {
+      setIsBottom(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div>
