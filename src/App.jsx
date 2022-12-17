2acc4d3ea11fff4d1a086fa897fe8491bb490193
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-12-17 14:48:57 +0300

    First commit --comitting all files into the repo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './assets/app.css'
+
+export default function App() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-center underline">App2</h1>
+    </div>
+  )
+}
